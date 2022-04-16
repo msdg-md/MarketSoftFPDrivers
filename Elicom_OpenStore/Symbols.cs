727cbccb0559d108fe930250b9e8793f6dc5862c
Dmitry Domnin <d.dmitry@market-soft.md>
2022-04-16 10:49:14 +0300

    Renamed OpenStore driver folder. Adjusted for old lib version

---
+++ Elicom_OpenStore/Symbols.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoenix.Devices.Printers.Elicom
+{
+    internal class Symbols
+    {
+        public const byte ACK = 0x06;
+        public const byte ETX = 0x0A;
+        public const byte NAK = 0x15;
+        public const byte RETRY = 0x0E;
+        public const byte STX = 0x02;
+    }
+}
+
+
+
+
+
+
+
+
+                                        
+
